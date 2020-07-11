c92c4b016406b9699ee3c32b20ca0a5f38b8e981
root <root@194-67-108-218.cloudvps.regruhosting.ru>
2020-07-12 00:16:41 +0300

    blog2 v0.0.16 review with async/await functions, upgrade header

--- nodemon.js
+++ nodemon.js
@@ -1,10 +1,10 @@
-{
-  "ignore": ["node_modules", "dev", "public"],
-  "env": {
-    "NODE_ENV": "development",
-    "PORT": 3000,
-    "MONGO_URL": "mongodb://localhost/blog"
-  },
-  "ext": ".js,.json",
-  "watch": "./"
-}
+// {
+//   "ignore": ["node_modules", "dev", "dest", "public"],
+//   "env": {
+//     "NODE_ENV": "development",
+//     "PORT": 3000,
+//     "MONGO_URL": "mongodb://localhost/blog"
+//   },
+//   "ext": ".js,.json",
+//   "watch": "./"
+// }
--- routes/archive.js
+++ routes/archive.js
@@ -4,37 +4,40 @@ const router = express.Router();
 const config = require('../config');
 const models = require('../models');
 
-function posts(req, res) {
+async function posts(req, res) {
   const userId = req.session.userId;
   const userLogin = req.session.userLogin;
   const perPage = +config.PER_PAGE;
   const page = req.params.page || 1;
 
-  models.Post.find({})
-    .skip(perPage * page - perPage)
-    .limit(perPage)
-    .then(posts => {
-      models.Post.countDocuments()
-        .then(count => {
-          res.render('index', {
-            posts,
-            current: page,
-            pages: Math.ceil(count / perPage),
-            user: {
-              id: userId,
-              login: userLogin
-            }
-          });
-        })
-        .catch(console.log);
-    })
-    .catch(console.log);
+  try {
+    const posts = await models.Post.find({})
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .populate('owner')
+      .sort({ createdAt: -1 });
+
+    const count = await models.Post.countDocuments();
+
+    res.render('index', {
+      posts,
+      current: page,
+      pages: Math.ceil(count / perPage),
+      user: {
+        id: userId,
+        login: userLogin
+      }
+    });
+
+  } catch (error) {
+    throw new Error('Server Error');
+  }
 }
 
 router.get('/', (req, res) => posts(req, res));
 router.get('/archive/:page', (req, res) => posts(req, res));
 
-router.get('/posts/:post', (req,res,next)=>{
+router.get('/posts/:post', async (req, res, next) => {
   const url = req.params.post.trim().replace(/ +(?= )/g, '');
   const userId = req.session.userId;
   const userLogin = req.session.userLogin;
@@ -45,9 +48,11 @@ router.get('/posts/:post', (req,res,next)=>{
     err.status = 404;
     next(err);
   } else {
-    models.Post.findOne({
-      url
-    }).then(post => {
+    try {
+      const post = models.Post.findOne({
+        url
+      });
+
       if (!post) {
         const err = new Error('Not Found');
         err.status = 404;
@@ -61,51 +66,51 @@ router.get('/posts/:post', (req,res,next)=>{
           }
         });
       }
-    });
+    } catch (error) {
+      throw new Error('Server Error');
+    }
   }
 });
 
 // users posts
-router.get('/users/:login/:page*?', (req, res) => {
+router.get('/users/:login/:page*?', async (req, res) => {
   const userId = req.session.userId;
   const userLogin = req.session.userLogin;
   const perPage = +config.PER_PAGE;
   const page = req.params.page || 1;
   const login = req.params.login;
 
-  models.User.findOne({
-    login
-  }).then(user => {
-    models.Post.find({
+  try {
+    const user = await models.User.findOne({
+      login
+    });
+
+    const posts = await models.Post.find({
       owner: user.id
     })
       .skip(perPage * page - perPage)
       .limit(perPage)
-      .sort({ createdAt: -1 })
-      .then(posts => {
-        models.Post.count({
-          owner: user.id
-        })
-          .then(count => {
-            res.render('archive/user', {
-              posts,
-              _user: user,
-              current: page,
-              pages: Math.ceil(count / perPage),
-              user: {
-                id: userId,
-                login: userLogin
-              }
-            });
-          })
-          .catch(() => {
-            throw new Error('Server Error');
-          });
-      })
-      .catch(() => {
-        throw new Error('Server Error');
-      });
-  });
+      .sort({ createdAt: -1 });
+
+    const count = await models.Post.count({
+      owner: user.id
+    });
+
+    res.render('archive/user', {
+      posts,
+      _user: user,
+      current: page,
+      pages: Math.ceil(count / perPage),
+      user: {
+        id: userId,
+        login: userLogin
+      }
+    });
+
+  } catch (error) {
+    throw new Error('Server Error');
+  }
+
 });
 
 module.exports = router;
